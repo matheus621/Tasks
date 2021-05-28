6c408f447bb2ffa80654c04234451e08b80bb565
Matheus Souza <matheus.souza>
2021-05-28 12:15:17 -0300

    Atualização service task

---
+++ app/src/main/java/com/example/tasks/service/model/PriorityModel.java
@@ -0,0 +1,12 @@
+package com.example.tasks.service.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PriorityModel {
+
+    @SerializedName("Id")
+    private int id;
+
+    @SerializedName("Description")
+    private String description;
+}
